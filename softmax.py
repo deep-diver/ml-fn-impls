03bf58cf52c0a3512ad21c68cf8e519c91ded64f
Chansung Park <deep.diver.csp@gmail.com>
2019-07-18 09:22:08 +0900

    avoiding max explosion

--- softmax.py
+++ softmax.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 def softmax(one_demension_array):
-  exps = np.exp(one_demension_array)
+  max = np.max(one_demension_array)
+  exps = np.exp(one_demension_array - max)
   exps_sum = np.sum(exps)
   
   y = exps / exps_sum
