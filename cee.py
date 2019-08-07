3356070898e2a2a06e106062bbbc4c7e68012fa2
Chansung Park <deep.diver.csp@gmail.com>
2019-08-07 23:06:47 +0900

    add batch_size

--- cee.py
+++ cee.py
@@ -1,5 +1,10 @@
 import numpy as np
 
 def cross_entropy_error(preds, labels):
+  if preds.ndim == 1:
+    preds = preds.reshape(1, preds.size)
+    labels = labels.reshape(1, labels.size)
+  
+  batch_size = preds.shape[0]
   delta = 1e-7
-  return -np.sum(labels*np.log(preds + delta))
+  return -np.sum(np.log(preds[np.arange(batch_size), labels] + delta)) / batch_size
